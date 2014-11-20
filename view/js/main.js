0020a6b79e7007242e4d46bb1f2009004dd322f6
syu93 <herve.tutuaku@gmail.com>
2014-11-21 00:55:31 +0100

    [feature] new load new page

--- view/js/main.js
+++ view/js/main.js
@@ -1,11 +1,27 @@
-var nb_sheet="1";
+var nb_sheet=1;
+var S_BODY_H=864;
 
 $(document).ready(function(){
 	$('#sheet-head_'+nb_sheet).prop('contenteditable',true);
 	$('#sheet-body_'+nb_sheet).prop('contenteditable',true); $('#sheet-body_'+nb_sheet).focus();
 	$('#sheet-footer_'+nb_sheet).prop('contenteditable',true);
+	var height = $('#sheet-body_'+nb_sheet).height();
+	console.log(height);
+	$('#sheet-body_'+nb_sheet).keyup(function(){
+		height = $('#sheet-body_'+nb_sheet).height();
+		if(height > S_BODY_H){
+			newpage(nb_sheet);
+		}
+	});
 });
 
-function newpage(){
+function newpage(nb_sheet){
+	var new_sheet = nb_sheet+1;
+	$('#sheet_'+nb_sheet).after('<div id="sheet_'+new_sheet+'" class="sheet"></div>');
+	$('#sheet_'+new_sheet).load( "view/tpl/sheet.tpl",{page:nb_sheet} );
 	
+	$('#temp-head_').attr('id', '#sheet-head_');
+	// $('#temp-head_').attr('id', '#sheet-head_'+new_sheet);
+	// $('#temp-body_').attr('id', '#sheet-body_'+new_sheet);
+	// $('#temp-footer_').attr('id', '#sheet-footer_'+new_sheet);
 }
---
+++ view/tpl/sheet.tpl
@@ -0,0 +1,16 @@
+	<!-- <div id="sheet_" class="sheet"> -->
+		<div class="container-sheet-head">
+			<div id="temp-head_" class="sheet-head">
+			</div>
+		</div>
+
+		<div class="container-sheet-body">
+			<div id="temp-body_" class="sheet-body">
+			</div>
+		</div>
+
+		<div class="container-sheet-footer">
+			<div id="temp-footer_" class="sheet-footer">
+			</div>
+		</div>
+	<!-- </div> -->
